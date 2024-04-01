8aba5d84a4972d3b727244495b0626b6c902b88a
Bendy545 <130989123+Bendy545@users.noreply.github.com>
2024-04-01 16:45:35 +0200

    Creating game loop and FPS check

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,12 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
+    private int frames = 0;
+    private float xDelta = 100, yDelta = 100;
+    private float xDir = 1f, yDir = 1f;
+    private long lastCheck = 0;
+    private Color color = new Color(30,55,90);
+    private Random random;
     public GamePanel() {
 
+        random = new Random();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
@@ -15,11 +22,11 @@ public class GamePanel extends JPanel {
         }
         public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
+
     }
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
+
     }
 
     public void setRectPos(int x, int y) {
@@ -29,7 +36,31 @@ public class GamePanel extends JPanel {
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect( xDelta, yDelta,200,50);
+        updateRectangle();
+        g.setColor(color);
+        g.fillRect( (int)xDelta, (int)yDelta,200,50);
+
+    }
+    private void updateRectangle() {
+        xDelta += xDir;
+        if (xDelta > 400 || xDelta < 0) {
+            xDir *= -1;
+            color = getRndColor();
+        }
+        yDelta += yDir;
+        if (yDelta > 400 || yDelta < 0) {
+            yDir *= -1;
+            color = getRndColor();
+        }
+
+
+    }
+
+    private Color getRndColor() {
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+        return new Color(r ,b ,g );
     }
 
 }
