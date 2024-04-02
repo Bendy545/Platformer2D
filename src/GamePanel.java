89cfeb69e35240f08fa6f5f4610b1e2ff642a7ae
Bendy545 <130989123+Bendy545@users.noreply.github.com>
2024-04-02 20:32:39 +0200

    Created main charackter sprite and cleaned up some methods in classes

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,25 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
+
 
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private int frames = 0;
     private float xDelta = 100, yDelta = 100;
-    private float xDir = 1f, yDir = 1f;
-    private long lastCheck = 0;
-    private Color color = new Color(30,55,90);
-    private Random random;
+
+
+
     public GamePanel() {
 
-        random = new Random();
+
         mouseInputs = new MouseInputs(this);
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
 
         }
+
+
         public void changeXDelta(int value) {
         this.xDelta += value;
 
@@ -36,32 +38,14 @@ public class GamePanel extends JPanel {
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        updateRectangle();
-        g.setColor(color);
-        g.fillRect( (int)xDelta, (int)yDelta,200,50);
-
-    }
-    private void updateRectangle() {
-        xDelta += xDir;
-        if (xDelta > 400 || xDelta < 0) {
-            xDir *= -1;
-            color = getRndColor();
-        }
-        yDelta += yDir;
-        if (yDelta > 400 || yDelta < 0) {
-            yDir *= -1;
-            color = getRndColor();
-        }
-
 
     }
 
-    private Color getRndColor() {
-        int r = random.nextInt(255);
-        int g = random.nextInt(255);
-        int b = random.nextInt(255);
-        return new Color(r ,b ,g );
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
     }
-
 }
 
