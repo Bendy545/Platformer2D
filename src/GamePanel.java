2074572031ed23ff32ec9165ed5b7a0f6a8c5a01
Bendy545 <130989123+Bendy545@users.noreply.github.com>
2024-04-03 20:51:54 +0200

    first walking animation

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -9,7 +9,9 @@ import java.io.InputStream;
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private float xDelta = 100, yDelta = 100;
-    private BufferedImage img, subImg;
+    private BufferedImage img;
+    private BufferedImage[][] animations;
+    private int animTick, animIndex, animSpeed = 30;
 
 
     public GamePanel() {
@@ -17,12 +19,22 @@ public class GamePanel extends JPanel {
 
         mouseInputs = new MouseInputs(this);
         importImg();
+        loadAnimations();
         setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
 
         }
+
+    private void loadAnimations() {
+        animations = new BufferedImage[4][3];
+        for (int j = 0;j < animations.length; j++) {
+        for (int i = 0; i < animations[j].length; i++) {
+            animations[j][i] = img.getSubimage(i * 32, j * 32, 32, 32);
+        }
+        }
+    }
 
     private void importImg() {
         InputStream is = getClass().getResourceAsStream("/Player_sprites.png");
@@ -31,6 +43,12 @@ public class GamePanel extends JPanel {
             img = ImageIO.read(is);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            try {
+            is.close();
+        } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -52,16 +70,29 @@ public class GamePanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        subImg = img.getSubimage(32,96,32,32);
-        g.drawImage(subImg, (int)xDelta, (int)yDelta,96,96, null);
+
+        updateAnimationTick();
+
+        g.drawImage(animations[1][animIndex], (int)xDelta, (int)yDelta,96,96, null);
+
+    }
+
+    private void updateAnimationTick() {
+
+        animTick++;
+        if (animTick >= animSpeed) {
+            animTick = 0;
+            animIndex++;
+            if (animIndex >= 3) {
+                animIndex = 0;
+            }
+        }
 
     }
 
     private void setPanelSize() {
         Dimension size = new Dimension(1280, 800);
-        setMinimumSize(size);
         setPreferredSize(size);
-        setMaximumSize(size);
     }
 }
 
