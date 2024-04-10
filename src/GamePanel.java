c927ec0da9de31616c619bbfa1ad6c9cac3aea35
Bendy545 <130989123+Bendy545@users.noreply.github.com>
2024-04-10 21:16:57 +0200

    Added new main character sprite sheet with new animations. Walking left, attacking left, falling.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -32,7 +32,7 @@ public class GamePanel extends JPanel {
         }
 
     private void loadAnimations() {
-        animations = new BufferedImage[4][3];
+        animations = new BufferedImage[7][3];
         for (int j = 0;j < animations.length; j++) {
         for (int i = 0; i < animations[j].length; i++) {
             animations[j][i] = img.getSubimage(i * 32, j * 32, 32, 32);
@@ -41,7 +41,7 @@ public class GamePanel extends JPanel {
     }
 
     private void importImg() {
-        InputStream is = getClass().getResourceAsStream("/Player_sprites.png");
+        InputStream is = getClass().getResourceAsStream("/PlayerSpriteSheet2.png");
 
         try {
             img = ImageIO.read(is);
@@ -97,7 +97,7 @@ public class GamePanel extends JPanel {
     private void setAnimation() {
 
         if (moving) {
-            playerAction = RUNNING;
+            playerAction = RUNNINGRIGHT;
         } else {
             playerAction = IDLE;
         }
--- src/utilz/Constants.java
+++ src/utilz/Constants.java
@@ -9,20 +9,29 @@ public class Constants {
         public static final int DOWN = 3;
     }
     public static class PlayerConstants {
-        public static final int RUNNING = 1;
+        public static final int RUNNINGRIGHT= 1;
+        public static final int RUNNINGLEFT = 4;
         public static final int IDLE = 0;
         public static final int JUMP = 2;
-        public static final int ATTACK = 3;
+        public static final int ATTACKRIGHT = 3;
+        public static final int ATTACLEFT = 6;
+        public static final int FALLING = 5;
 
         public static int GetSpriteAmount(int player_action) {
             switch (player_action) {
                 case IDLE:
                     return 2;
-                case RUNNING:
+                case RUNNINGRIGHT:
                     return 3;
                 case JUMP:
                     return 3;
-                case ATTACK:
+                case ATTACKRIGHT:
+                    return 2;
+                case RUNNINGLEFT:
+                    return 3;
+                case FALLING:
+                    return 2;
+                case ATTACLEFT:
                     return 2;
                 default:
                     return 1;
