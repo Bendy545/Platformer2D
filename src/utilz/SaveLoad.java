c701699f5160abd3e42ed5b148a768203622189a
Bendy545 <130989123+Bendy545@users.noreply.github.com>
2024-04-21 14:40:19 +0200

    Added LevelSprite.

--- src/utilz/SaveLoad.java
+++ src/utilz/SaveLoad.java
@@ -8,7 +8,7 @@ import java.io.InputStream;
 public class SaveLoad {
 
     public static final String PLAYER_ATLAS = "PlayerSpriteSheet2.png";
-    public static final String LEVEL_ATLAS = "PlayerSpriteSheet2.png";
+    public static final String LEVEL_ATLAS = "LevelSprite.png";
 
     public static BufferedImage GetSpriteAtlas(String fileName) {
         BufferedImage img = null;
